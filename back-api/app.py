50606f21a09a8bc3647e71d606328f0138a30e0a
azaharacc <cerezo.azahara@gmail.com>
2025-06-21 14:57:50 +0200

    header

--- back-api/app.py
+++ back-api/app.py
@@ -15,7 +15,7 @@ def get_db():
     conn = psycopg2.connect(os.getenv("DATABASE_URL"))
     return conn
 
-def validar_api_key(x_api_key: str = Header(None)):
+def validar_api_key(x_api_key: str = Header(...)):
     if x_api_key != API_KEY:
         raise HTTPException(status_code=401, detail="Unauthorized")
 
